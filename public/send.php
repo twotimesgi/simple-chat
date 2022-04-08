7589a2f49d487e30d6e31d7710cdcbdc08e95326
twotimesgi <luigibardellagerbi@mail.com>
2022-04-08 23:23:58 +0200

    Fix

--- public/send.php
+++ public/send.php
@@ -1,4 +1,18 @@
 <?php
-    $_GET["msg"];
-    $_GET["user"];
+
+$msg = Array (
+        "sender" =>  $_GET["sender"],
+        "text" => $_GET["text"],
+        "time" => date('m/d/Y H:i:s', time())
+);
+
+// encode array to json
+$json = json_encode(array('data' => $msg));
+
+//write json to file
+if (file_put_contents("messages.json", $json))
+    echo "JSON file created successfully...";
+else 
+    echo "Oops! Error creating json file...";
+
 ?>
