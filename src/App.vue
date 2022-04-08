6ef513473ac068526b30ddb64603bb5485d36ded
twotimesgi <luigibardellagerbi@mail.com>
2022-04-08 18:27:37 +0200

    Fix

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,87 @@
 <template>
 <div id="app">
-  <chat-box/>
+  <status-bar :logged="logged" :user="user"/>
+    <div v-if="!logged" class="login-box">
+      <div>
+      <h4>Scegli il tuo username: </h4>
+      <input placeholder="username" v-model="user" type="text">
+      <button @click="login">Login</button>
+      </div>
+    </div>
+    <message-box v-if="logged" :userId="userId"/>
+    <input-box v-if="logged" />
 </div>
 </template>
 
 <script>
-import ChatBox from './components/ChatBox.vue'
+import InputBox from './InputBox.vue';
+import MessageBox from "./MessageBox.vue";
+import StatusBar from "./StatusBar.vue";
 
 export default {
   name: 'App',
-  components: {
-    ChatBox
+  components:{
+    MessageBox,
+    StatusBar,
+    InputBox
   },
+  data(){
+    return {
+      logged: false,
+      user: "",
+      userId: null
+    }
+  },
+  mounted(){
+    setInterval(()=>{
+      console.log("fetchmsg");
+    }, 1000)
+  },
+  methods:{
+    login(){
+      this.logged = true;
+    }
+  }
 }
 </script>
 
-<style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap');
-
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+#app{
+  width: 700px;
+  height: 500px;
+  background-color: rgb(252, 252, 252);
+  position: relative;
 }
 
-body{
-  font-family: 'Roboto', sans-serif;
-}
-
-#app {
-  height: 100vh;
+.login-box{
   width: 100%;
-  background: linear-gradient(90deg, rgba(40,40,40,1) 0%, rgba(28,28,28,1) 100%);
+  height:425px;
   display: flex;
   justify-content: center;
   align-items: center;
+
+  div{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  }
+
+  input{
+      margin: 10px 0;
+  }
+}
+
+
+.chat::before {
+  content: "";
+  height: 5px;
+  background: rgb(0, 210, 123);
+  background: linear-gradient(90deg, rgba(1,255,150,1) 0%, rgba(0,210,123,1) 100%);
+  position: absolute;
+  top: -5px;
+  left: 0;
+  right: 0;
 }
 </style>
